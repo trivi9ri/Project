379df2c5235f269bdd142ca709912870cbf9af76
trivi9cri <trivi9cri@gmail.com>
2016-02-17 14:52:26 +0900

    put date(unixtime) to tsdb

---
+++ put_unix_time.py
@@ -0,0 +1,44 @@
+﻿import time
+import datetime
+import json
+import urllib2
+from types import *
+import requests
+
+url = "http://swcon.asuscomm.com:64242/api/put"
+
+
+def ostdb_restful_push():
+	curtime = int(time.time())
+	sname = "yung"
+	data = {
+		"metric": "syb.put_unixtime",
+		"timestamp": time.time(),
+		"value": curtime,
+		"tags": {
+			"sensor": "none",
+			"name": sname,
+			"floor_room": "6F_office",
+			"building": "global_rnd_center",
+			"owner": "yung",	
+			"country": "kor"
+		}
+	}
+
+	try: 
+		ret = requests.post(url, data = json.dumps(data))
+		print str(curtime)
+		print ret
+	except requests.exceptions.Timeout:
+		logger.error("http connection error, Timeout %s",ret)
+		print "Timeout Exception"
+	except requests.exceptions.ConnectionError:
+		logger.error("http connection error, Too many requests")
+		print "Connection error"
+		pass
+	return 
+
+if __name__ == "__main__":
+	while (1):
+		ostdb_restful_push()
+		time.sleep(5)
